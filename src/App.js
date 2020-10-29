a5eb3e602e7472ae2beff09227c338bccf1a5dc5
Amber Moroe <ambermoorexc@gmail.com>
2020-10-28 22:15:05 -0500

    Function to create each character as a '1' in an array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, {useState} from 'react'
 import './App.css';
 
-// Starting
+
 function App() {
+  const [string, setString] = useState('')
+  setString('Amber')
+
+const strToNum = (str) => {
+  const regex = /[a-z]/g;
+return str.toLowerCase().split('').map((item, index) => item.match(regex) ?
+item[index] = 1 : 
+  item)
+}
+console.log(strToNum(string))
   return (
     <div className="App">
       
