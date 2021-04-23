7284f4c41e649d1dd49e1051de6c6fcb7453ec90
ForkingPaths2040 <amberm@smallwoodhome.com>
2021-04-23 00:24:37 -0500

    Finished basic functionality

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,22 +1,25 @@
 import React, {useState} from 'react';
-import {input, convertToNums} from '../scratch.js'
+import * as name from '../scratch.js'
 
 function Result(props) {
   const {name1, name2} = props
-  const [full, setFull] = useState('');
-  const [numbers, setNumbers] = useState();
+  const [result, setResult] = useState(null);
+  
   
 
   const handleSubmit=()=>{
-    setFull(input(name1, name2))
+    let characters = (name.convertToChars(name.input(name1, name2)))
+    let nums = name.find_duplicate_in_array(characters)
+    let percentage = name.percentage(nums)
+    setResult(percentage.join('') + '%')
+
   }
   
   return (
     <div className='flex-column align-center'>
       <button onClick={handleSubmit} style={{ fontFamily: 'Montserrat', fontSize: '22px', margin: '25px 0' }}>Enter</button>
-      <p style={{ fontFamily: 'Montserrat', fontSize: '48px' }}>68%</p>
-      <p>{full}</p>
-      <p>{numbers}</p>
+      <p style={{ fontFamily: 'Montserrat', fontSize: '48px' }}>{result}</p>
+      
     </div>
   );
 }
--- src/scratch.js
+++ src/scratch.js
@@ -5,16 +5,16 @@ export function input(name1, name2) {
 
 // input("Nathan", "Caitlin");
 
-export function convertToNums(input) {
+export function convertToChars(input) {
   const arr = input.split("");
   return arr;
 }
 
-const example = convertToNums(input("Nathan", "Caitlin"));
+// const example = convertToChars(input("Nathan", "Caitlin"));
 
-function find_duplicate_in_array(array) {
+export function find_duplicate_in_array(array) {
   const count = {};
-  const result = [];
+  // const result = [];
 
   array.forEach((item) => {
     if (count[item]) {
@@ -30,28 +30,33 @@ function find_duplicate_in_array(array) {
   return nums;
 }
 
-const numbers = find_duplicate_in_array(example);
-console.log(numbers);
+// const numbers = find_duplicate_in_array(example);
+// console.log(numbers);
 
-function percentage(nums) {
+export function percentage(nums) {
   const reduce = [];
   if (nums.length === 2) {
-    console.log(`${nums.join("")}%`);
-  } else if (nums.length % 2 !== 0) {
-    for (let i = 0; nums.length > 1; i++) {
-      console.log(i);
-      reduce.push(nums.shift() + nums.pop());
-      console.log(reduce);
-    }
-    reduce.push(nums[0]);
-    console.log(reduce);
+    console.log("two", nums);
+    return nums;
   } else {
-    for (let i = 0; nums.length > 0; i++) {
-      console.log(i);
-      reduce.push(nums.shift() + nums.pop());
-      console.log(reduce);
+    if (nums.length % 2 !== 0) {
+      for (let i = 0; nums.length > 1; i++) {
+        // console.log(i);
+        reduce.push(nums.shift() + nums.pop());
+        console.log("odd", reduce);
+      }
+      reduce.push(nums[0]);
+      console.log("odd", reduce);
+      return percentage(reduce);
+    } else {
+      for (let i = 0; nums.length > 0; i++) {
+        // console.log(i);
+        reduce.push(nums.shift() + nums.pop());
+        console.log("even", reduce);
+      }
+      return percentage(reduce);
     }
   }
-  percentage(reduce);
 }
-// percentage(numbers);
+// const result = percentage(numbers);
+// console.log(result.join("") + "%");
